bb642c4ff49ad9fd56529476b1652892813d2296
edoecohen <edoecohen@gmail.com>
2015-05-01 20:11:33 -0700

    Adds Procfile.

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ app.get('/', function (req, res) {
   
 });
 
-
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
