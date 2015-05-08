03e02e0a5de6c7d6dffff71b1ce31a077a32780d
edoecohen <edoecohen@gmail.com>
2015-05-08 15:42:32 -0700

    Makes changes to config.js

--- server/config.js
+++ server/config.js
@@ -14,19 +14,24 @@
 
 // module.exports = db;
 
-
+var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 if (!global.hasOwnProperty('db')) {
   var Sequelize = require('sequelize')
     , sequelize = null
 
   if (process.env.HEROKU_POSTGRESQL_BLACK_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BLACK_URL, {
+    sequelize = new Sequelize(match[5], match[1], match[2], {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     5432,
-      host:     'ec2-184-73-253-4.compute-1.amazonaws.com',
-      logging:  true //false
+      // port:     5432,
+      // host:     'ec2-184-73-253-4.compute-1.amazonaws.com',
+      port:     match[4],
+      host:     match[3],
+      logging:  true,
+      dialectOptions: {
+        ssl: true
+      }
     })
   } else {
     // the application is executed on the local machine ... use mysql
