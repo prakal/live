194b907f856773bad95e7109d5dd7ba86e9f765c
edoecohen <edoecohen@gmail.com>
2015-05-08 20:35:40 -0700

    Removes all comments from schema files

--- server/models/artists.js
+++ server/models/artists.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Artist", {
-    // tableName: 'artists',
+    tableName: 'artists',
     timestamps: true,
     artist_id: { 
       type: DataTypes.INTEGER,
@@ -21,96 +21,3 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 }
-
-
-// Artist.sync({force: true}).then(function () {
-//   return Artist.create({
-//     artistName: 'U2',
-//     artistPic: 'https://i.scdn.co/image/bbf6684084d0f444b92789abcdb66bb16c7d2c43',
-//     genre: 'Rock',
-//     avgRating: 5,
-//     bio: 'U2 are an Irish rock band from Dublin. Formed in 1976, the group consists of Bono (vocals and guitar), The Edge (guitar, keyboards, and vocals), Adam Clayton (bass guitar), and Larry Mullen Jr.'
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Metallica',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 3
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Blink 182',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'KISS',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 2
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Lil Wayne',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 1
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Shit Band',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Diana Ross',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Nelly',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Amy Winehouse',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'Sum 41',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// })
-// .then(function () {
-//   return Artist.create({
-//     artistName: 'The Strokes',
-//     artistPic: 'https://i.scdn.co/image/15d9ccb37a96d49f69f44f999fb1e9e46f9b3ccd',
-//     genre: 'Hard Rock',
-//     avgRating: 4
-//   });
-// });
-
-// module.exports = Artist;
--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,7 +1,3 @@
-// var db = require('../config');
-// var Sequelize = require('sequelize');
-// var Artist = require('./artists.js')
-
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Review", {
     tableName: 'reviews',
@@ -18,45 +14,3 @@ module.exports = function(sequelize, DataTypes) {
     textReview: DataTypes.TEXT
   })
 }
-
-
-// Review.sync({force: true}).then(function () {
-//   // Table created
-//   return Review.create({
-//     username: 'Mark Martinez',
-//     artistName: 'U2',
-//     rating: 4,
-//     venue: 'Madison Square Garden, NYC',
-//     textReview: 'I really loved seeing U2 in concert! They are incredible!!!'
-//   });
-// })
-// .then(function () {
-//   // Table created
-//   return Review.create({
-//     username: 'Rob Stark',
-//     artistName: 'U2',
-//     rating: 5,
-//     venue: 'SAP Center, San Jose',
-//     textReview: 'U2 live! What more can I say!! Way better than the red wedding!'
-//   });
-// })
-// .then(function () {
-//   // Table created
-//   return Review.create({
-//     username: 'Jamie Lannister',
-//     artistName: 'Amy Winehouse',
-//     rating: 2,
-//     venue: 'The Kitchen',
-//     textReview: 'One of her last gigs. Wish I could say I enjoyed it. But I did not.'
-//   });
-// })
-// .then(function () {
-//   // Table created
-//   return Review.create({
-//     rating: 1,
-//     venue: 'Madison Squgsdgare Gardegsd, NYC',
-//     textReview: 'I reallgsdgsy loved gsdgsseeing U2 in concert! They are incredible!!!'
-//   });
-// });
-
-// module.exports = Review;
