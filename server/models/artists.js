34593be904fcf3f66e114cf25b54acb3430ec58c
edoecohen <edoecohen@gmail.com>
2015-05-09 15:53:27 -0700

    Comments out all the schemas

--- server/models/artists.js
+++ server/models/artists.js
@@ -1,22 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Artist", {
-    timestamps: true,
-    artist_id: { 
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    artistName: {
-      type: DataTypes.STRING,
-      unique: true
-    },
-    artistPic: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    bio: DataTypes.TEXT,
-    avgRating: DataTypes.INTEGER,
-    reviewCount: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0    
-    }
+    // timestamps: true,
+    // artist_id: { 
+    //   type: DataTypes.INTEGER,
+    //   autoIncrement: true,
+    //   primaryKey: true
+    // },
+    // artistName: {
+    //   type: DataTypes.STRING,
+    //   unique: true
+    // },
+    // artistPic: DataTypes.STRING,
+    // genre: DataTypes.STRING,
+    // bio: DataTypes.TEXT,
+    // avgRating: DataTypes.INTEGER,
+    // reviewCount: {
+    //   type: DataTypes.INTEGER,
+    //   defaultValue: 0    
+    // }
   });
 }
--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,15 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define("Review", {
-    timestamps: true,
-    artistName: DataTypes.STRING,
-    username: DataTypes.STRING,
-    review_id: { 
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    venue: DataTypes.STRING,
-    rating: DataTypes.INTEGER,
-    textReview: DataTypes.TEXT
-  })
+    // timestamps: true,
+    // artistName: DataTypes.STRING,
+    // username: DataTypes.STRING,
+    // review_id: { 
+    //   type: DataTypes.INTEGER,
+    //   autoIncrement: true,
+    //   primaryKey: true
+    // },
+    // venue: DataTypes.STRING,
+    // rating: DataTypes.INTEGER,
+    // textReview: DataTypes.TEXT
+  });
 }
