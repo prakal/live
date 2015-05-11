2b2cdd1dd3f8ba9ffdbb8d20ca8472d69267ddf0
edoecohen <edoecohen@gmail.com>
2015-05-11 08:43:48 -0700

    Refactored server for getAvgRating

--- server/server.js
+++ server/server.js
@@ -61,9 +61,9 @@ var query = ' \
 ';
 
 app.get('/getAvgRating', function(req, res){
-  // db.sequelize.query("SELECT AVG(rating) FROM `Reviews` WHERE artistName = ? ", {replacements: [req.query.artistName], type: sequelize.QueryTypes.SELECT})
-  db.sequelize.query(query, null, {raw: true}, { 
-  artistName: req.query.artistName 
+  db.sequelize.query("SELECT AVG(rating) FROM `Reviews` WHERE artistName = :artistName ", {replacements: [req.query.artistName], type: sequelize.QueryTypes.SELECT})
+  // db.sequelize.query(query, null, {raw: true}, { 
+  // artistName: req.query.artistName 
 })
   .then(function(avgRating) {
     console.log('average rating: ', avgRating[0]);
