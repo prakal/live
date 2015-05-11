35cf9e93a1c85deee950f78d665b67ab22d30548
edoecohen <edoecohen@gmail.com>
2015-05-11 11:32:21 -0700

    Changes spelling of avgRating to avgrating

--- server/server.js
+++ server/server.js
@@ -68,9 +68,9 @@ app.get('/getAvgRating', function(req, res){
   db.sequelize.query(query, null, {raw: true}, { 
   artistName: req.query.artistName
 })
-  .then(function(avgRating) {
-    console.log('average rating: ', avgRating[0]);
-    res.status(200).json(avgRating[0]);
+  .then(function(avgrating) {
+    console.log('average rating: ', avgrating[0]);
+    res.status(200).json(avgrating[0]);
   });
 });
 
@@ -98,10 +98,10 @@ app.post('/newreview', function(req, res) {
 });
 
 app.post('/updateAvgRating', function(req, res) {
-  console.log('avgRating:', req.body.avgRating);
+  console.log('avgrating:', req.body.avgrating);
   db.Artist
     .update( {
-      avgRating: req.body.avgRating,
+      avgrating: req.body.avgrating,
       reviewcount: sequelize.literal('reviewcount + 1')
     },
     { where: 
